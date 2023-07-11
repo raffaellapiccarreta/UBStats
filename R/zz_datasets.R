737d6159c3cfb1defa44ac03077aae1df6de553b
sergioventurini <sergio.venturini@gmail.com>
2023-07-11 16:35:39 +0200

    Add vignette and other features
    Vignette and other features added

--- R/zz_datasets.R
+++ R/zz_datasets.R
@@ -1,45 +1,66 @@
 #' Data: MktDATA
 #'
-#' This dataset contains the variables from a customer satisfaction study of 
-#' a Spanish credit institution on 250 customers. The data is identical to
-#' the dataset provided by the \href{https://github.com/gastonstat/plspm/}{plspm} package
-#' but with the last column  (`gender`) removed. If you are looking for the original
-#' dataset use the [MktDATA] dataset.
+#' This dataset is a modification of the original link{\code{MktDATA.Orig}}
+#' dataset and it is provided for user convenience.
 #'
-#' @docType data
+#' @usage data(MktDATA)
 #'
-#' @description A data frame with 250 observations and 27 variables. 
-#' Variables from 1 to 27 refer to six latent concepts: \code{IMAG}=Image, 
-#' \code{EXPE}=Expectations, \code{QUAL}=Quality, \code{VAL}=Value, 
-#' \code{SAT}=Satisfaction, and \code{LOY}=Loyalty.
-#' \describe{
-#'   \item{imag1-imag5}{Indicators attached to concept `IMAG` which is supposed to
-#'                      capture aspects such as the institutions reputation, 
-#'                      trustworthiness, seriousness, solidness, and caring 
-#'                      about customer.}
-#'   \item{expe1-expe5}{Indicators attached to concept `EXPE` which is supposed to
-#'                      capture aspects concerning products and 
-#'                      services provided, customer service, providing solutions,
-#'                      and expectations for the overall quality.}
-#'   \item{qual1-qual5}{Indicators attached to concept `QUAL` which is supposed to
-#'                      capture aspects concerning reliability of products and services, 
-#'                      the range of products and services, personal advice, 
-#'                      and overall perceived quality.}
-#'   \item{val1-val4}{Indicators attached to concept `VAL` which is supposed to
-#'                      capture aspects related to beneficial services and 
-#'                      products, valuable investments, quality relative to 
-#'                      price, and price relative to quality.}
-#'   \item{sat1-sat4}{Indicators attached to concept `SAT` which is supposed to
-#'                      capture aspects concerning overall rating of satisfaction, 
-#'                      fulfillment of expectations, satisfaction relative to 
-#'                      other banks, and performance relative to customer's 
-#'                      ideal bank.}
-#'   \item{loy1-loy4}{Indicators attached to concept `LOY` which is supposed to
-#'                    capture aspects concerning propensity to choose the 
-#'                    same bank again, propensity to switch to other bank, 
-#'                    intention to recommend the bank to friends, 
-#'                    and the sense of loyalty.}
-#' }
+#' @description A data frame with 2224 observations and 26 variables.
+"MktDATA"
+
+#' Data: MktDATA.Orig
+#'
+#' This dataset contains the variables from a survey on a set of customers
+#' of a company operating in the retail food sector. The company sells
+#' products from 3 major categories (referred to as A, B, C) The customers can
+#' order and acquire products in the company physical stores, or through the
+#' company's website (in this case, they can order on the website and pick up the
+#' order in one store). Information is collected on customers' activity in the
+#' last two years (observation period), as well as some information retrieved
+#' through questionnaires or fidelity cards. During such period different
+#' marketing strategies were adopted to improve customers' fidelization, and 5
+#' marketing campaigns were launched; a last campaign was launched at the end of
+#' the observation period.
+#'
+#' @usage data(MktDATA.Orig)
+#'
+#' @description A data frame with 2224 observations and the following 19
+#' variables (levels of the variables listed in alphabetical order):
+#' 
+#' * **`CustId`** (num): customer's identification label
+#' * **`Gender`** (chr): customer's gender (`F`, `M`)
+#' * **`Age`** (num): customer's age (in years)
+#' * **`Education`** (chr): customer's level of education(`College`, `Graduate`,
+#    `HighSchool`,`Post-Grad`)
+#' * **`Marital_Status`** (chr): customer's marital status
+#    (`Divorced`, `Married`, `Single`, `Together`, #' `Widow`)
+#' * **`Children`** (num): number of children in the household
+#' * **`Kids`** (num): number of kids aged less than 12 in the
+#'   household
+#' * **`Income`** (chr): customer's income (measured in classes)
+#' * **`Baseline`** (num): index (from 0 to 1) assigned by the marketing dept
+#    indicating how promising the #' customer was judged at the beginning of
+#    the observation period
+#' * **`LikeMost`** (chr): Most frequently bought category in the last two
+#    years (`P.A`, `P.B`, `P.C`)
+#' * **`TotVal`** (num): amount spent in the last 2 years
+#' * **`NPickUp_Purch`** (num): number of purchases made through company's
+#    website and picked up in physical #' store  
+#' * **`NWeb_Purch`** (num): number of purchases made through company's website
+#    and delivered at home
+#' * **`NStore_Purch`** (num): number of purchases made in a physical store
+#' * **`NDeals`** (num): number of products purchases with discount
+#' * **`CustClass`** (chr): customer's classification (assigned by the marketing
+#    dept) based on past #' profitability (`Bronze`, `Gold`, `Platinum`,
+#    `Silver`)
+#' * **`PastCampaigns`** (num): number of offers accepted by the customer in the
+#    last 2 years' marketing #' campaigns
+#' * **`LastCampaign`** (num): binary variable (0/1) indicating whether (1) or
+#    not (0) the customer accepted #' the offer in the campaign launched at
+#    the end of the observation period
+#' * **`WouldSuggest`** (chr): variable signalling whether (`Yes`) or not
+#    (`No`) the customer declared they would suggest the company's products
+#    to friends and family
 #' 
 #' @source The data set has been adapted from \href{https://www.kaggle.com/code/dmitryuarov/customers-clustering-eda}{https://www.kaggle.com/code/dmitryuarov/customers-clustering-eda}.
-"MktDATA"
+"MktDATA.Orig"
