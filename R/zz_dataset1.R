9da3fa826e82d837c3cad4b60a07337b3a01ce6c
sergioventurini <sergio.venturini@gmail.com>
2023-07-12 12:27:12 +0200

    Update documentation
    Documentation updated

--- R/zz_dataset1.R
+++
@@ -1,9 +0,0 @@
-#' Data: MktDATA
-#'
-#' This dataset is a modification of the original \code{\link{MktDATA.Orig}}
-#' dataset and it is provided for user convenience.
-#'
-#' @usage data(MktDATA)
-#'
-#' @format{A data frame with 2224 observations and 26 variables.}
-"MktDATA"
